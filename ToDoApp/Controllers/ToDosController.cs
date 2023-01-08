33e1e51642dcfd15d09855b20fde1fdd5faab628
Orhan Bilge <orhan@bilge.pro>
2023-01-09 00:05:10 +0300

    Creating To-Do list page.

---
+++ ToDoApp/Controllers/ToDosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoApp.Controllers
+{
+    [Authorize]
+    public class ToDosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
